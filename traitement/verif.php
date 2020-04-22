4e56ca3301c42a480f704bd550b6e95b25052620
Sanghelyos <pecheux@simplon-charleville.fr>
2020-04-22 10:35:49 +0200

    voir readme

--- traitement/verif.php
+++ traitement/verif.php
@@ -7,40 +7,12 @@ require '../include/classes.php';
 $identifiant = !empty($_POST['identifiant']) ? $_POST['identifiant'] : NULL;
 $mdp = !empty($_POST['password']) ? $_POST['password'] : NULL;
 $mdp = md5($mdp);
+$user = new UserMin($identifiant, $mdp);
+$utilisateur = $user->login($bdd);
 
-if ($identifiant != NULL || $mdp != NULL)
-{
 
-    if ($_SESSION['sess'] == NULL)
-    {
-        $user = new UserLogin($identifiant, $mdp);
-        $utilisateur = $user->login($bdd);
-
-        if ($utilisateur['identifiant'] == $identifiant && $utilisateur['mdp_utilisateur'] == $mdp)
-        {
-            $_SESSION['sess'] = $utilisateur['id_utilisateur'];
-            $_SESSION['iden'] = $utilisateur['identifiant'];
-            $_SESSION['date'] = date("d-m-Y",strtotime($utilisateur['date_creation']));
-            $_SESSION['type'] = $utilisateur['id_type'];
-            header('Location: ../connland.php?err=1');
-            exit();
-        }
-        else
-        {
-            header('Location: ../connexion.php?err=2');
-            exit();
-        }
         
-    }
-    else
-    {
-             header('Location: ../connland.php?err=3');
-            exit();
-    }
-}
-else
-{
-    header('Location: ../connland.php?err=4');
-    exit();
-}
+        
+
+
 ?>
